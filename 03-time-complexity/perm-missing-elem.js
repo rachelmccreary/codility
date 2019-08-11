3c8c4c4e3f3d4e76da7279b77e2663efc07059b8
Rachel McCreary <rachel.exposure@gmail.com>
2019-08-11 12:07:53 -0400

    perm missing element

---
+++ 03-time-complexity/perm-missing-elem.js
@@ -0,0 +1,39 @@
+/*
+
+PermMissingElem
+
+https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+function solution(A);
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+
+*/
+function solution(A) {
+    let set1 = new Set(A);
+    for (let i = 1; i <= A.length + 1; i++) {
+        if (!set1.has(i)) {
+            return i;
+        }
+    }
+}
